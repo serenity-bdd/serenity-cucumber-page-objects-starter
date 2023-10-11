49c6441b365bc1a852721420d9a5fe3684dd85da
John Ferguson Smart <john.smart@wakaleo.com>
2023-10-11 07:23:00 +0100

    Initial commit

---
+++ src/test/java/starter/CucumberTestSuite.java
@@ -0,0 +1,15 @@
+package starter;
+
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("/features")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "io.cucumber.core.plugin.SerenityReporterParallel,pretty,timeline:build/test-results/timeline")
+public class CucumberTestSuite {
+}
---
+++ src/test/java/starter/navigation/DuckDuckGoHomePage.java
@@ -0,0 +1,8 @@
+package starter.navigation;
+
+import net.serenitybdd.annotations.DefaultUrl;
+import net.thucydides.core.pages.PageObject;
+
+@DefaultUrl("https://duckduckgo.com")
+public class DuckDuckGoHomePage extends PageObject {
+}
